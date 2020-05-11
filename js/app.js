2d193267ef7c59d25adb556c1a1c2f3193a21a47
Gian Franco <gianfrancogv@gmail.com>
2020-05-10 23:54:16 -0300

    Guardando cambios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 let descripcion = document.getElementById("descripcion");
 let fichaTecnica = document.getElementById("ficha-tecnica");
+let btnCompra = document.querySelector("#boton-compra");
+let parent = document.querySelector(".compra");
+let corazon = document.querySelector(".like2");
+let sobre = document.querySelector(".envelope");
+let carrito = document.querySelector(".cart");
+
+let spinner = document.createElement('div');
+let texto = document.createElement('div');
 
 descripcion.addEventListener('click', function(){
     this.style.color = '#b6180d';
@@ -14,3 +22,32 @@ fichaTecnica.addEventListener('click', function(){
     descripcion.style.color = 'black';
     descripcion.style.borderBottom = 'none';
 });
+
+btnCompra.addEventListener('click', addSpinner);
+
+function addSpinner() {
+    texto.innerHTML = 'CARGANDO DATOS';
+    texto.setAttribute("class", "cargando-datos");
+    spinner.setAttribute("class", "spinner-border text-danger");
+    parent.replaceChild(spinner, btnCompra);
+    parent.appendChild(texto);
+    parent.style.marginLeft = "10px";
+    corazon.remove();
+    sobre.remove();
+    setTimeout(removeSpinner, 2000);
+}
+
+function productoAgregado() {
+    let productoAgregado = document.createElement('div');
+    parent.appendChild(productoAgregado);
+    productoAgregado.setAttribute("class", "cargando-datos");
+    productoAgregado.innerHTML = '¡Producto agregado al carrito!';
+    carrito.removeAttribute("src");
+    carrito.setAttribute("src", "img/cart1.png");
+}
+
+function removeSpinner() {
+    spinner.remove();
+    texto.remove();
+    productoAgregado();
+}
